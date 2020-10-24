90f03ef77b9395a8f33c10eb4a1f0a4cc229f6e4
Britton Kowalk <britton.kowalk@divvypay.com>
2020-10-24 16:18:10 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ynab-checker
+
+Sends an email for one of your category groups in YNAB, outlining how much is available that day if you want to stay on pace for your goal.
---
+++ checker.js
@@ -0,0 +1,79 @@
+var request = require('request');
+var config = require('config')
+var nodemailer = require('nodemailer');
+
+var emailBody = ""
+
+//configure email service
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.get("fromEmail"),
+        pass: config.get("fromEmailPass")
+    }
+});
+
+//configure YNAB API request
+var requestOptions = {
+    url: 'https://api.youneedabudget.com/v1/budgets/' + config.get("budgetID") + '/categories',
+    headers:{'Authorization': 'Bearer ' + config.get("authToken")}
+};
+
+function todaysDate() {
+    var now = new Date()
+    return now.getDate()
+  }
+
+function daysInThisMonth() {
+    var now = new Date()
+    return new Date(now.getFullYear(), now.getMonth()+1, 0).getDate()
+  }
+  
+function sendEmail(){
+    transporter.sendMail({
+       from: config.get("fromEmail"),
+       bcc: config.get("toEmail"),
+       subject: "Budget Balances",
+       html: emailBody
+    }, function(error, response){
+        if(error){
+            console.log(error);
+        } else {
+            console.log("Budget message sent.");
+            transporter.close();
+        }
+    });
+}
+
+function processCategories(group){
+    group.categories.forEach(function(category){
+        name = category.name
+        goal = category.goal_target/1000
+        available = (category.budgeted + category.activity)/1000
+        goalPerDay = goal / daysInThisMonth()
+
+        usedOfGoal = goal - available // Need this instead of just spent amount so pacing doesn't change if you move money from another category.
+        pacedSpendAmountSoFar = todaysDate() * goalPerDay // If we were exactly on pace, how much we would have spent by today.
+        availableToday = Math.round(pacedSpendAmountSoFar - usedOfGoal) // How much we can spend today and stay on pace
+        availableTodayZeroMin = (availableToday < 0) ? 0 : availableToday // Don't display a negative amount for today's paced amount, just a zero.
+        daysSavedUp = Math.round(availableToday/goalPerDay) -1 // How many days of spending we've saved up (or negative for days behind)
+
+        emailBody += "<h1 style='margin:0'>" + name + " - $" + availableTodayZeroMin + "</h1>"
+                   + "<p style='margin-top:10px'>(" + daysSavedUp + " days saved up)</p>"
+                   + "<p style='margin-bottom:60px;'>$" + Math.round(available) + " of $" + goal + " available"
+    })
+}
+
+function processCategoriesJsonAndEmail(error, response, body){
+    if(!error){
+        budget = JSON.parse(body)
+        budget.data.category_groups.forEach(function(group){
+            if(group.name == config.get("categoryGroupName")){
+                processCategories(group)
+            }
+        });
+        sendEmail()
+    }
+}
+
+request(requestOptions, processCategoriesJsonAndEmail)
